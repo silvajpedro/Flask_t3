85bb143f81291c2cc52bce32bddbe95d5a7fae71
silvajpedro <belojpedro.31@gmail.com>
2025-02-26 20:38:38 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# Importa a classe Flask do pacote flask
+from flask import Flask
+
+# Cria uma instância do Flask, que será a base do nosso aplicativo web
+app = Flask(__name__)
+
+# Define uma rota para o endpoint "/devedora"
+# Isso significa que quando o usuário acessar "http://127.0.0.1:5000/devedora", essa função será executada
+@app.route('/devedora')
+def pague():
+    # Retorna uma resposta em HTML para ser exibida no navegador
+    return "<h1>Quem não deve, não teme!</h1>"
+
+# Inicia o servidor Flask para rodar a aplicação localmente
+# O servidor será executado em "http://127.0.0.1:5000/"
+app.run()
